86b4c9a3679f619600b20b294f1efb53f286fe4a
juliomr1 <juliomurillo.rodriguez@gmail.com>
2023-03-24 18:24:03 -0500

    puliendo el boot

--- boot.py
+++ boot.py
@@ -1,11 +1,34 @@
+import uasyncio as asyncio
 from machine import Pin
-import time 
+import network 
+import time
+import urequests
 
-p2 = Pin(4, Pin.OUT)
-p2.on()
+p1 = Pin(2, Pin.OUT)
+P2 = Pin(4, Pin.OUT)
 
-while(1):
-    p2.on()
-    time.sleep_ms(1000)
-    p2.off()
-    time.sleep_ms(1000)
+async def daemon_wifi():
+    wifi = network.WLAN(network.STA_IF)
+    timeout = 0
+    wifi.active(False)
+    await asyncio.sleep(0.5)
+    wifi.active(True)
+    wifi.connect('TE VI PERRO','1083019146')
+    while True:
+        print("Daemon___0")
+        if not wifi.isconnected():
+            print('connecting...')
+            while (not wifi.isconnected() and timeout < 5):
+                # print(5 - timeout)
+                 timeout = timeout + 1
+                 await asyncio.sleep(1)
+        if wifi.isconnected():
+            print('connected')
+        else:
+            print('Not Connected')
+        await asyncio.sleep(10)
+
+loop = asyncio.get_event_loop()
+loop.create_task(daemon_wifi())
+#loop.create_task(daemon1())
+loop.run_forever()
